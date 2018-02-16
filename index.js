1bb51c82c2bcd6412a83dac55537bfb110adcc19
MD5HashBrowns <md5hashbrownz@gmail.com>
2018-02-15 19:43:16 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+'use strict';
+const BootBot = require('bootbot');
+
+const bot = new BootBot({
+  accessToken: 'FB_ACCESS_TOKEN',
+  verifyToken: 'FB_VERIFY_TOKEN',
+  appSecret: 'FB_APP_SECRET'
+});
